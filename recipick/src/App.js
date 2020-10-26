bbd8dd8c26e090d36c8eee1ad09bf827f05c04a0
root <root@DESKTOP-2TOP005.localdomain>
2020-10-27 05:55:32 +0900

    implemented category search page

--- recipick/src/App.js
+++ recipick/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 //Local imports:
 import './App.css';
 import Mainpage from './containers/Mainpage/Mainpage';
+import RecipeList from './containers/RecipeList/RecipeList'
 import Navbar from './containers/Navbar/Navbar';
 
 function App(props) {
@@ -13,6 +14,7 @@ function App(props) {
       <div className="App">
         <Navbar history={props.history}/>
         <Switch>
+          <Route path='/search' exact component={RecipeList} />
           <Route path='/main-page' exact render={() => <Mainpage history={props.history}/>} />
           <Redirect from='/' exact to='/main-page'/>
           <Route render = {() => <h1>Not Found</h1>} />
