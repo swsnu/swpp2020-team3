227731bcc83afa349670df2174757d37d4d76028
Hyunsoo Kim <hscornelia@snu.ac.kr>
2020-11-09 05:43:49 +0900

    Commit

--- frontend/src/store/reducers/recipe.js
+++ frontend/src/store/reducers/recipe.js
@@ -10,17 +10,12 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.GET_RECIPES:
       return { ...state, recipes: action.recipes };
-<<<<<<< HEAD
-
     case actionTypes.GET_RECIPE:
       return { ...state, selectedRecipe: action.recipe};
-
-=======
     case actionTypes.CREATE_RECIPE:
       return {...state};
     case actionTypes.GET_INGREDIENTS:
       return {...state, ingredientList: action.ingredients}
->>>>>>> master
     default:
       break;
   }
