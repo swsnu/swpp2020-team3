db119919a8db431bf23a935ad745ef082708aef4
Hyunsoo Kim <hscornelia@snu.ac.kr>
2020-11-13 06:12:10 +0900

    remove unnecessary comment

--- frontend/src/containers/Createpage/Createpage.test.js
+++ frontend/src/containers/Createpage/Createpage.test.js
@@ -21,27 +21,6 @@ const stubState = {
 const history = createBrowserHistory()
 const mockStore = getMockStore(stubState)
 
-function MockFile() {}
-
-MockFile.prototype.create = function(name, size, mimeType) {
-    name = name || 'mock.txt';
-    size = size || 1024;
-    mimeType = mimeType || 'plain/txt';
-
-    function range(count) {
-        var output = '';
-        for (var i = 0; i < count; i++) {
-            output += 'a';
-        }
-        return output;
-    }
-
-    var blob = new Blob([range(size)], { type: mimeType });
-    blob.lastModifiedDate = new Date();
-    blob.name = name;
-
-    return blob;
-};
 jest.mock('./CreateStep', () => {
     return jest.fn((props) => {
         let inputHandler = (event) => {
