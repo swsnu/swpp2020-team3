965cdd8ee475bf42742cece5d208bd681884fa83
root <root@DESKTOP-905SA40.localdomain>
2020-11-14 08:58:16 +0900

    added Replies/Reply test files

---
+++ frontend/src/containers/reply/Reply.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Reply from './Reply';
+
+describe('<Reply />', () => {
+  it('should render without errors', () => {
+    const component = shallow(<Reply />);
+    const wrapper = component.find(".reply");
+    expect(wrapper.length).toBe(1);
+  });
+
+  it('should handle deleteReply', () => {
+    const mockClickDelete = jest.fn();
+    const component = shallow(<Reply onDeleteReply={mockClickDelete} />);
+    const wrapper = component.find('#delete-reply-button');
+    wrapper.simulate('click');
+    expect(mockClickDelete).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle editReply 1', () => {
+    const spyPrompt = jest.spyOn(window, 'prompt').mockImplementation(() => 'yes');
+    const mockClickEdit = jest.fn();
+    const component = shallow(<Reply onEditReply={mockClickEdit}/>);
+    const wrapper = component.find('#edit-reply-button');
+    wrapper.simulate('click');
+    //expect(component.onEdit()).toHaveBeenCalledTimes(1);
+    expect(mockClickEdit).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle editReply 2', () => {
+    const spyPrompt = jest.spyOn(window, 'prompt').mockImplementation(() => '');
+    const mockClickEdit = jest.fn();
+    const component = shallow(<Reply onEditReply={mockClickEdit}/>);
+    const wrapper = component.find('#edit-reply-button');
+    wrapper.simulate('click');
+    //expect(component.onEdit()).toHaveBeenCalledTimes(1);
+    expect(mockClickEdit).toHaveBeenCalledTimes(0);
+  });
+  
+});
