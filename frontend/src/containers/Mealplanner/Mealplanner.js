e634e32fb82b40730dc5ebd2d30e28d31a26225b
Hyunsoo Kim <hscornelia@snu.ac.kr>
2020-11-21 18:29:24 +0900

    drag and drop implemented

--- frontend/src/containers/Mealplanner/Mealplanner.js
+++ frontend/src/containers/Mealplanner/Mealplanner.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
@@ -16,47 +16,65 @@ const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
-  
+
     return result;
-  };
+};
 const unflatten = (arr, n) => {
-    arr.reduce((acc, entry, i) => (i % n ? acc[acc.length-1].push(entry) : acc.push([entry]),acc), [])
+    arr.reduce((acc, entry, i) => (i % n ? acc[acc.length - 1].push(entry) : acc.push([entry]), acc), [])
 }
 const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
-    arr.slice(i * size, i * size + size)
-  );
-  function splitArrayIntoChunksOfLen(arr, len) {
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+        arr.slice(i * size, i * size + size)
+    );
+function splitArrayIntoChunksOfLen(arr, len) {
     var chunks = [], i = 0, n = arr.length;
     while (i < n) {
-      chunks.push(arr.slice(i, i += len));
+        chunks.push(arr.slice(i, i += len));
     }
     return chunks;
-  }
+}
 const reorder2 = (list, startIndex, endIndex) => {
     console.log(list)
     let result = Array.from(list).flat()
-    let temp = result[startIndex-1]
-    result[startIndex-1] = result[endIndex-1]
-    result[endIndex-1] = temp
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    // let temp = result[startIndex-1]
+    // result[startIndex-1] = result[endIndex-1]
+    // result[endIndex-1] = temp
     result = chunk(result, 3)
     console.log(result)
     return result;
 }
-class Mealplanner extends Component{
-    constructor(props){
+
+/////
+const isFull = (list) => {
+    let temp = list.filter((element) => element.value != 0)
+    return temp.length
+}
+const updateIndex = (list) => {
+    let newIndex = 1;
+    let temp = list.flat()
+    temp.map((element) => {
+        element.index = newIndex
+        newIndex++
+    })
+}
+
+// TODO: get articles and display thumbnail
+class Mealplanner extends Component {
+    constructor(props) {
         super(props)
         this.onDragEnd = this.onDragEnd.bind(this);
     }
-    state={
+    state = {
         min: '',
         max: '',
         numOfDays: '',
-        recipeArray: [[{index: 1, value: 0}, {index: 2, value: 0}, {index: 3, value: 0}], 
-        [{index: 4, value: 0}, {index: 5, value: 0}, {index: 6, value: 0}], 
-        [{index: 7, value: 0}, {index: 8, value: 0}, {index: 9, value: 0}]],
-        scrappedRecipes: [{id: 1, thumbnail: 'hana'}, {id: 2, thumbnail: 'duna'}, 
-        {id: 3, thumbnail: 'sena'}, {id: 4, thumbnail: 'nena'}]
+        recipeArray: [[{ index: 1, value: 0 }, { index: 2, value: 0 }, { index: 3, value: 0 }],
+        [{ index: 4, value: 0 }, { index: 5, value: 0 }, { index: 6, value: 0 }],
+        [{ index: 7, value: 0 }, { index: 8, value: 0 }, { index: 9, value: 0 }]],
+        scrappedRecipes: [{ id: 1, thumbnail: 'hana' }, { id: 2, thumbnail: 'duna' },
+        { id: 3, thumbnail: 'sena' }, { id: 4, thumbnail: 'nena' }]
     }
     componentDidMount() {
         // Will remove this and replace with reducer that gets ml recommendation
@@ -67,89 +85,99 @@ class Mealplanner extends Component{
             minDuration: 0, maxDuration: 1000,
             pageStart: 0,
         }
-        this.props.getRecipes(toEraseQuery).then((res) => console.log(res))
+        this.props.getRecipes(toEraseQuery).then((res) => console.log('"retrieved recipes"'))
         // this.props.getScrappedRecipes().then((res) => this.setState({scrappedRecipes: res.data})) // not implemented yet
     }
-    
-    onDragEnd(result){
-        console.log(result)
-        const {source, destination} = result
-        if(!destination){
-            return;
+
+    addDayAbove(index) {
+        if (this.state.recipeArray.length >= 7) {
+            console.log('max number is 7')
         }
-        if(source.droppableId == destination.droppableId){
-            if(source.droppableId == 'droppableId_cookbook'){
-                const items = reorder(
-                    this.state.scrappedRecipes,
-                    source.index,
-                    destination.index
-                );
-                this.setState({scrappedRecipes: items});
-            }
-            else{
-                const items = reorder2(
-                    this.state.recipeArray,
-                    source.index,
-                    destination.index
-                );
-                console.log(source.index)
-                console.log(destination.index)
-                this.setState({recipeArray: items});
-            }
-        }
-        else{ // one list to another
-            console.log('corner case')
+        else {
+            let insertSubList = [{ index: null, value: 0 }, { index: null, value: 0 }, { index: null, value: 0 }]
+            let list = this.state.recipeArray;
+            list.splice(index, 0, insertSubList)
+            updateIndex(list)
+            this.setState({ recipeArray: list })
         }
     }
 
-    render(){
-        return(
+    onDragEnd(result) {
+        const { source, destination } = result
+        // no effect movements:
+        if (!destination || destination.droppableId == 'scrappedArticles' 
+            || (source.droppableId.split('day').length >= 2 && destination.droppableId=='scrappedArticles')
+            || (source.droppableId.split('day').length >= 2 && destination.droppableId.split('day').length >= 2)) {
+            console.log('no movements')
+            return;
+        }
+        if (source.droppableId == destination.droppableId) {
+            console.log('no case')
+        }
+        else { // one list to another
+            console.log(result);
+            let recipeArray = this.state.recipeArray;
+            let selected = this.state.scrappedRecipes[source.index]
+            let d_id = destination.droppableId.split('day')[1]
+            let m_id = destination.index
+            let modify = recipeArray.splice(d_id, 1)
+            let toswap = modify[0].splice(m_id, 1)
+            toswap[0].value = selected.thumbnail
+            selected = toswap[0]
+            modify[0].splice(m_id, 0, selected)
+            recipeArray.splice(d_id, 0, modify[0])
+            this.setState({ recipeArray: recipeArray })
+        }
+    }
+
+    render() {
+        console.log(this.state.recipeArray)
+        return (
             <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppableId_recipes">
-                {(provided) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                        {this.state.recipeArray && this.state.recipeArray.map((dayBlock, ind) => (
-                            <div className='day' key={ind}>
+                {this.state.recipeArray && this.state.recipeArray.map((dayBlock, ind) => (
+                    <Droppable droppableId={`day${ind}`} key={ind} >
+                        {(provided) => (
+                            <div className='day' ref={provided.innerRef} {...provided.droppableProps}>
                                 <button onClick={() => this.addDayAbove(ind)}>Add a day</button>
                                 <button onClick={() => this.deleteDay(ind)}>Delete day</button>
                                 {dayBlock && dayBlock.map((meal, index) => (
-                                    <Draggable key={meal.index} draggableId={JSON.stringify(meal)} index={meal.index}>
+                                    <Draggable draggableId={`day_${ind}/meal_${index}`} index={index} key={index} >
                                         {(provided) => (
-                                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                <div className='singleBlock' key={meal.index}>
-                                                    <p>{meal.index}</p>
-                                                </div> 
+                                            <div  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
+                                                <div className='singleBlock' >
+                                                    <p>{meal.value == undefined ? meal.thumbnail : meal.value}</p>
+                                                </div>
                                             </div>
                                         )}
                                     </Draggable>
                                 ))}
-                        {provided.placeholder}
+                                {provided.placeholder}
                             </div>
-                        ))}
-                    </div>
-                )}
-                </Droppable>
-                <Droppable droppableId="droppableId_cookbook">
-                {(provided) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                        {this.state.scrappedRecipes && this.state.scrappedRecipes.map((recipe, index) => (
-                            <Draggable key={recipe.id} draggableId={recipe.thumbnail} index={index}>
-                                {(provided) => (
-                                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                        <div className='scrappedRecipe'>
-                                            <p>{recipe.thumbnail}</p>
+                        )}
+
+                    </Droppable>
+                ))}
+                <Droppable droppableId="scrappedArticles">
+                    {(provided) => (
+                        <div {...provided.droppableProps} ref={provided.innerRef}>
+                            {this.state.scrappedRecipes && this.state.scrappedRecipes.map((recipe, index) => (
+                                <Draggable key={recipe.id} draggableId={`recipe_${recipe.id}`} index={index}>
+                                    {(provided) => (
+                                        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                            <div className='scrappedRecipe'>
+                                                <p>{recipe.thumbnail}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
             </DragDropContext>
         )
-    
+
     }
 }
 
@@ -158,7 +186,7 @@ const mapStateToProps = state => {
         storedRecipes: state.rcp.recipes,
     }
 }
-  
+
 const mapDispatchToProps = dispatch => {
     return {
         getRecipes: (searchSettings) =>
@@ -172,4 +200,4 @@ Mealplanner.propTypes = {
     // history: PropTypes.object
 };
 
-export default  connect(mapStateToProps,mapDispatchToProps)(withRouter(Mealplanner));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Mealplanner));
