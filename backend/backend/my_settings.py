1a8803583b0864237cf0629e03293c3094ee1e1f
Hyunsoo Kim <hscornelia@snu.ac.kr>
2020-11-28 07:53:22 +0900

    remove all password

--- backend/backend/my_settings.py
+++ backend/backend/my_settings.py
@@ -1,6 +1,6 @@
-# import json
-# with open('credentials.json') as json_data:
-#     credentials = json.load(json_data)
+import json
+with open('backend/credentials.json') as json_data:
+    credentials = json.load(json_data)
 # credentials = {'word': 'swpp2020'}
 
 # Email settings
@@ -10,6 +10,6 @@ EMAIL = {
 'EMAIL_PORT' : 587,
 'EMAIL_HOST' : 'smtp.gmail.com',   
 'EMAIL_HOST_USER' : 'swppsend@gmail.com',                    
-# 'EMAIL_HOST_PASSWORD' : '', # changed to root
+'EMAIL_HOST_PASSWORD' : credentials['password'], # changed to root
 'REDIRECT_PAGE' : 'http://10.58.5.40:3000/signin'
 }
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-# with open('credentials.json') as json_data:
-#     credentials = json.load(json_data)
+with open('credentials.json') as json_data:
+    credentials = json.load(json_data)
 
 # credentials = {'password': 'swpp2020'}
 
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'recipick_database',
         'USER': 'root',
-        # 'PASSWORD': credentials['password'],
+        'PASSWORD': credentials['password'],
         'HOST': 'localhost',
         'PORT': '',
     }
