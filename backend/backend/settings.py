1fc80c9a446022dd10ccaf9b61fd32e03a7c679d
Ubuntu <ubuntu@ip-172-31-17-214.ec2.internal>
2020-12-02 05:39:12 +0000

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@$(7l20qx*j1rqu9=s@=a3ey3pmn1wa5+n_85+b&075eis0%@$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.217.98.184']
 
 
 # Application definition
