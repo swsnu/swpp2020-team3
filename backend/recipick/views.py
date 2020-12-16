3ef555e31525bd89ddd86c62c9ddf77f9b2480ae
creeper00 <rnjswldnd@snu.ac.kr>
2020-12-16 12:04:24 +0900

    ml

--- backend/recipick/views.py
+++ backend/recipick/views.py
@@ -676,22 +676,24 @@ def recipe_rating(request, id):
         recipe.save()
         response = {'user.id': user.id, 'rating': previous.rating, 'recipe.id': recipe.id}
 
-        timestamp = int(time.time())
+        i = body['rating']-2
+        for j in range(1,i):
+            timestamp = int(time.time())
 
-        personalize_events = boto3.client('personalize-events', 'us-east-1')
+            personalize_events = boto3.client('personalize-events', 'us-east-1')
 
-        data = personalize_events.put_events(
-            trackingId = 'dea1262b-0b85-409e-8ee0-f94ec208e665',
-            userId = user.id,
-            sessionId = 'session_id',
-            eventList = [{
-                'sentAt': timestamp,
-                'eventType': 'Likes',
-                'properties': json.dumps({
-                'itemId': recipe.id
-                })
-            }]
-        )
+            data = personalize_events.put_events(
+                trackingId = 'dea1262b-0b85-409e-8ee0-f94ec208e665',
+                userId = user.id,
+                sessionId = 'session_id',
+                eventList = [{
+                    'sentAt': timestamp,
+                    'eventType': 'Likes',
+                    'properties': json.dumps({
+                    'itemId': recipe.id
+                    })
+                }]
+            )
 
 
         return JsonResponse(response, safe=False, status=200)
